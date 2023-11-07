d34c9766d6fdcbd5f0dd06e2f7a283bca77a942d
rafigarcia <rafaela.garcia.angulo@gmail.com>
2023-11-07 15:45:40 -0600

    W01 Developer-C# Programming

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1,101);
+
+        int count = 0;
+        int guess = -1;
+
+        while (guess != magic)
+        {
+            count += 1;
+            
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        
+            if (magic > guess)
+            {
+                Console.WriteLine("Higher");
+            } 
+            else if (magic < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {count} times.");
+            }
+        }
+
+        
+        
+            
     }
 }
